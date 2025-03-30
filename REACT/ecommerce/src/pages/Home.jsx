7a75fc3bc496e3271610e4dff2f125b06c1892f2
GopalYad <yadavgopal6675@gmail.com>
2025-03-30 20:10:00 +0530

    learning routes and tailwindcss

---
+++ REACT/ecommerce/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChartCard from '../components/ChartCard'
+
+const Home = () => {
+  return (
+    <div>
+     <ChartCard/>
+    </div>
+  )
+}
+
+export default Home
